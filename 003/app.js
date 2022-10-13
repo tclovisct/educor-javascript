0331491ed9f22d4be0485c2c56e113cf583f0436
Clovis Tristao <tclovis@gmail.com>
2022-10-13 14:59:16 -0300

    Atualizações

---
+++ 003/app.js
@@ -0,0 +1,23 @@
+/* Operadores */
+
+let n1, n2
+
+n1 = 10
+n2 = 2
+
+document.write("N1 + N2 = " + n1+n2 + "<br>")
+document.write("N1 - N2 = " + n1-n2 + "<br>")
+document.write("N1 / N2 = " + n1/n2 + "<br>")
+document.write("N1 * N2 = " + n1*n2 + "<br")
+
+let nome, sobrenome
+nome = "João"
+sobrenome = "Paulo"
+
+document.write(nome + sobrenome + "<br>")
+
+document.write('N1 % N2 =' + n1%n2 + "<br>")
+
+document.write('N1 ** N2 = ' + n1**n2 + "<br")
+
+document.write(Math.sqrt(16))
